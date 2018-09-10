17c746b034dc67811580dd994d9e845fa3ad8816
jack <hao3304@qq.com>
2018-09-10 14:56:28 +0800

    国外精确到国家

--- src/config/crontab.js
+++ src/config/crontab.js
@@ -3,5 +3,10 @@ module.exports = [
         cron: '*/30 * * * *',
         handle: 'api/geo',
         immediate: true
+    },
+    {
+        cron: '*/20 * * * *',
+        handle: 'api/top',
+        immediate: true
     }
 ]
