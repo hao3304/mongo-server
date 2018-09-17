c1f2378f721fc49100264db0639575e6a3b3aedc
jack <hao3304@qq.com>
2018-09-17 10:52:56 +0800

    数据库测试

--- src/config/crontab.js
+++ src/config/crontab.js
@@ -1,12 +1,12 @@
 module.exports = [
-    {
-        cron: '*/20 * * * *',
-        handle: 'api/top',
-        immediate: true
-    },
-    {
-        cron: '*/30 * * * *',
-        handle: 'api/geo',
-        immediate: true
-    }
+    // {
+    //     cron: '*/20 * * * *',
+    //     handle: 'api/top',
+    //     immediate: true
+    // },
+    // {
+    //     cron: '*/30 * * * *',
+    //     handle: 'api/geo',
+    //     immediate: true
+    // }
 ]
